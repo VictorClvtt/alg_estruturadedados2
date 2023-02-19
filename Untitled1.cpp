ef173316f60c63953a7ea23bb7f65450b2bd5695
Victor Clivatti <joaoclivatti@hotmail.com>
2023-02-19 00:51:27 -0300

    ASCII UI Update

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -14,19 +14,28 @@ string pesquisa(funcionario funcionarios[]) {
     string res;
     int a, ddd1;
 
-    cout << "Digite o ddd dos registros que deseja ver: ";
+    cout << "+--------------------------------------------------+" << endl;
+    cout << "|         PESQUISA DE DADOS DE FUNCIONARIOS        |" << endl;
+    cout << "+--------------------------------------------------+" << endl;
+    cout << "|Digite o DDD dos registros que deseja ver:        |" << endl;
+    cout << "+--------------------------------------------------+" << endl;
     cin >> ddd1;
 
     for (a = 0; a < 3; a++) {
         if (ddd1 == funcionarios[a].ddd) {
-            cout << "FUNCIONARIO NUMERO " << a + 1 << ":" << endl;
-            cout << "Nome: " << funcionarios[a].nome << endl;
-            cout << "Telefone: " << funcionarios[a].ddd << funcionarios[a].tel << endl;
-            cout << "Cidade: " << funcionarios[a].cidade << endl;
+            cout << "+--------------------------------------------------+" << endl;
+            cout << "|             DADOS DO(S) FUNCIONARIO(S)           |" << endl;
+            cout << "+--------------------------------------------------+" << endl;
+            cout << "|Funcionario numero " << a + 1 << ":" << endl;
+            cout << "|Nome: " << funcionarios[a].nome << endl;
+            cout << "|Telefone: " << funcionarios[a].ddd << funcionarios[a].tel << endl;
+            cout << "|Cidade: " << funcionarios[a].cidade << endl << endl;
         }
     }
 
-    cout << "Deseja fazer outra pesquisa pelos dados com o ddd?" << endl;
+    cout << "+--------------------------------------------------+" << endl;
+    cout << "|Deseja fazer outra pesquisa pelos dados com o DDD?|" << endl;
+    cout << "+--------------------------------------------------+" << endl;
     cin >> res;
     cout << "\033[2J\033[1;1H";
 
@@ -39,37 +48,46 @@ int main() {
     string r;
     string resposta;
 
+
     for (i = 0; i < 3; i++) {
-        cout << "Digite o nome do funcionario numero " << i + 1 << ": ";
+        cout << "+--------------------------------------------------+" << endl;
+        cout << "|           SISTEMA DE REGISTRO DE FUNCIONARIOS    |" << endl;
+        cout << "+--------------------------------------------------+" << endl;
+        cout << "|             INSERCAO DE DADOS DE FUNCIONARIOS    |" << endl;
+        cout << "+--------------------------------------------------+" << endl;
+        cout << "|Funcionario " << i + 1 << ":" << endl;
+        cout << "|Nome: ";
         cin >> pessoa[i].nome;
-        cout << "\033[2J\033[1;1H";
-
-        cout << "Digite o ddd do funcionario numero " << i + 1 << ": ";
+        cout << "|DDD: ";
         cin >> pessoa[i].ddd;
-        cout << "\033[2J\033[1;1H";
-
-        cout << "Digite o telefone do funcionario numero " << i + 1 << ": ";
+        cout << "|Telefone: ";
         cin >> pessoa[i].tel;
-        cout << "\033[2J\033[1;1H";
-
-        cout << "Digite a cidade do funcionario numero " << i + 1 << ": ";
+        cout << "|Cidade: ";
         cin >> pessoa[i].cidade;
+        cout << "+--------------------------------------------------+" << endl;
         cout << "\033[2J\033[1;1H";
     }
 
-    cout << "Deseja fazer uma pesquisa pelos dados com o ddd?" << endl;
+    cout << "+--------------------------------------------------+" << endl;
+    cout << "|             PESQUISA DE DADOS DE FUNCIONARIOS    |" << endl;
+    cout << "+--------------------------------------------------+" << endl;
+    cout << "|Deseja fazer uma pesquisa pelos dados com o DDD?  |" << endl;
+    cout << "+--------------------------------------------------+" << endl;
     cin >> r;
     cout << "\033[2J\033[1;1H";
 
     if (r == "sim" || r == "Sim") {
         do {
             resposta = pesquisa(pessoa);
-        } while (resposta != "nao" && resposta != "Nao");
+        } while (resposta != "nao" || resposta != "Nao");
 
-        cout << "Ok, adeus :)";
-    } else if (r == "nao" || r == "Nao") {
-        cout << "Ok, adeus :)";
+        cout << "+--------------------------------------------------+" << endl;
+        cout << "|                     ADEUS!                       |" << endl;
+        cout << "+--------------------------------------------------+" << endl;
+    }else if (r == "nao" || r == "Nao"){
+        cout << "+--------------------------------------------------+" << endl;
+        cout << "|                     ADEUS!                       |" << endl;
+        cout << "+--------------------------------------------------+" << endl;
     }
-
     return 0;
 }
